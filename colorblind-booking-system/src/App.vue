41a652efd6b1fc4186ae5bb583e0575d9634df4d
LukeVassallo18 <124679236+LukeVassallo18@users.noreply.github.com>
2025-11-10 18:52:02 +0100

    Delete colorblind-booking-system directory

--- colorblind-booking-system/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <nav class="bg-gray-800 text-white p-4 flex space-x-4">
-    <RouterLink to="/" class="hover:underline">Home</RouterLink>
-    <RouterLink to="/about" class="hover:underline">About</RouterLink>
-  </nav>
-  <router-view />
-</template>
--- colorblind-booking-system/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './style.css' // ✅ or './assets/main.css' depending on your setup
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
